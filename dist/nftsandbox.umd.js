ff1079a8813b2e971415789d7b2d920cce6a900c
Simon Fremaux <dievardump@gmail.com>
2021-03-19 19:20:24 +0100

    0.0.8

--- dist/nftsandbox.umd.js
+++ dist/nftsandbox.umd.js
@@ -1,5 +1,3 @@
-
-(function(l, r) { if (l.getElementById('livereloadscript')) return; r = l.createElement('script'); r.async = 1; r.src = '//' + (window.location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1'; r.id = 'livereloadscript'; l.getElementsByTagName('head')[0].appendChild(r) })(window.document);
 (function (global, factory) {
     typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
     typeof define === 'function' && define.amd ? define(factory) :
@@ -949,7 +947,7 @@
     	let { owner_properties = {} } = $$props;
     	let { owner = "0x0000000000000000000000000000000000000000" } = $$props;
     	let { sandbox_props = "" } = $$props;
-    	const version = "0.0.7";
+    	const version = "0.0.8";
     	let proxy = null;
 
     	function getProxy() {
